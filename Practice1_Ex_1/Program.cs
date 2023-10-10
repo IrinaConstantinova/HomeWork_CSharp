48b683f00932fd38615786daaaad1c9f2f716192
Irina.Constantinova <irina.constantinova@gmail.com>
2023-10-10 14:25:14 +0300

    Added Practice1_Ex_1

---
+++ Practice1_Ex_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на входе принимает
+// два числа и выдаёт, какое число больше, а какое маньше.
+// a = 5; b = 7 -> Первое число 5 меньше чем второе число 7
+// a = 2; b = 2 -> Введённые числа равны
+// a = -3; b = -9 -> Первое число -3 больше чем второе число -9
+
+System.Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine("Первое число " + a + " больше чем второе число " + b);
+}
+else if (a < b)
+{
+    System.Console.WriteLine("Первое число " + a + " менише чем второе число " + b);
+}
+else if (a == b)
+{
+    System.Console.WriteLine("Введённые числа равны " + a);
+}
