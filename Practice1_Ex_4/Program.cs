6127a9063e6aaae527fb4e78e9ca4d8a294b015d
Irina.Constantinova <irina.constantinova@gmail.com>
2023-10-10 15:42:38 +0300

    Added Practice1_Ex_4

---
+++ Practice1_Ex_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход 
+// принимает число (N), а на выходе 
+// показывает все чётные число от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Чётные числа от 1 до " + N + ":");
+
+for ( int i = 2; i <= N; i+=2)
+{
+    System.Console.WriteLine(i);
+}
+
+
