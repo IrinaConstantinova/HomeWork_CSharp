3a21e140201171d954f412dd7eec3923f0f2bee4
Irina.Constantinova <irina.constantinova@gmail.com>
2023-10-24 19:21:46 +0300

    the last commit

--- Practice5_Ex1/Program.cs
+++ Practice5_Ex1/Program.cs
@@ -1,19 +1,16 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-
-// метод задания массива
-//             длина       миним. значение   макс. значение 
 int[] GenerateNumArray(int length, int minNumArray, int maxNumArray)
 {   
-    int[] array = new int[length]; // обьявили массив и его длину задаст пользователь
-    Random rand = new Random(); // задали метод генерации случайных чисел
+    int[] array = new int[length]; 
+    Random rand = new Random(); 
     
     for (int i = 0; i < length; i++)
     {
-        array[i] = rand.Next(minNumArray, maxNumArray + 1); // цикл заполнит массив случайными числами в диапазоне от минимального числа до максимального
+        array[i] = rand.Next(minNumArray, maxNumArray + 1); 
     }
-    return array; // вернёт массив
+    return array; 
 }
 
 void PrintArray(int[] arrayForPrint)  
