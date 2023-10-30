1b4612e7953c0d65c60278708f67f1dabf84ffaf
Irina.Constantinova <irina.constantinova@gmail.com>
2023-10-30 16:33:55 +0300

    Added Practice6_Ex1 and Ex2

---
+++ Practice6_Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}   
+
+System.Console.WriteLine("Введите числа разделённые пробелом: ");
+int[] newarray = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+System.Console.WriteLine($"Количество положительных чисел: {CountPositiveNumbers(newarray)}");
---
+++ Practice6_Ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System.Reflection.Metadata;
+
+double ReadNumbers(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void IntersectionPoint(double b1, double b2, double m1, double m2)
+{
+    double x;
+    double y;
+
+    x = (b2 - b1) / (m1 - m2);
+    y = m1 * x+ b1;
+
+    System.Console.WriteLine($"Точка пересечения прямых A и B: ({x} ; {y})");
+}
+
+double b1 = ReadNumbers("Введите точку пересесения прямой А с осью ординат b1 = ");
+double k1 = ReadNumbers("Введите угловой коэффициент прямой А k1 = ");
+double b2 = ReadNumbers("Введите точку пересесения прямой B с осью ординат b2 = ");
+double k2 = ReadNumbers("Введите угловой коэффициент прямой B k2 = ");
+
+
+IntersectionPoint(b1, b2, k1, k2);
+
